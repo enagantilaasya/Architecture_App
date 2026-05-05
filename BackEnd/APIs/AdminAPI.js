49f282a7dce7acc776d295896ce140f573c2b152
enagantilaasya <engantilaasya2006@gmail.com>
2026-05-05 14:20:09 +0530

    reinstall dependencies

--- BackEnd/APIs/AdminAPI.js
+++ BackEnd/APIs/AdminAPI.js
@@ -1,5 +1,5 @@
 import exp from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { UserModel } from '../models/UserModel.js'
 export const adminApp = exp.Router()
