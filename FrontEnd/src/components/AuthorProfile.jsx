5cf6082262ebdd5ce82b98606e8f8369435e9547
enagantilaasya <engantilaasya2006@gmail.com>
2026-05-05 13:51:44 +0530

    commited 3

--- FrontEnd/src/components/AuthorProfile.jsx
+++ FrontEnd/src/components/AuthorProfile.jsx
@@ -12,7 +12,7 @@ function AuthorProfile() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-cyan-50">
+    <div className="min-h-screen  from-indigo-50 via-white to-cyan-50">
 
       <div className="max-w-5xl mx-auto px-6 py-10">
 
--- FrontEnd/src/components/Home.jsx
+++ FrontEnd/src/components/Home.jsx
@@ -56,7 +56,7 @@ function Home() {
     );
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-cyan-50">
+    <div className="min-h-screen from-indigo-50 via-white to-cyan-50">
 
       <div className="max-w-6xl mx-auto px-6 py-10">
 
