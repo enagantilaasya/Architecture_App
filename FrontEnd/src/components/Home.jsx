165661a8f9f8ac7db81ab749f537f8c21acf3d8f
enagantilaasya <engantilaasya2006@gmail.com>
2026-05-13 13:03:58 +0530

    removed bottem scroll bar

--- FrontEnd/src/components/Home.jsx
+++ FrontEnd/src/components/Home.jsx
@@ -5,20 +5,21 @@ function Home() {
     <div>
 
       {/* hero section */}
-      <div className="relative w-screen h-screen overflow-hidden -mx-32">
+      <div className="relative overflow-hidden ">
 
         <img
           src={image}
           alt="img not available"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover "
         />
 
       
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6 mt-70">
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-6 mt-55 ">
 
           <NavLink
             to="/login"
-            className="bg-white text-stone-800 text-sm font-semibold  px-10 py-4 rounded-full hover:bg-stone-100 transition"
+            className="bg-white text-stone-800 text-sm font-semibold  
+            px-10 py-4 rounded-full hover:bg-stone-100 shadow-2xl shadow-slate-700 border-blue-100 border-2"
           >
             Start Reading 
           </NavLink>
